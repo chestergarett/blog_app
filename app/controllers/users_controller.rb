477f6ac73096983885173fe938726f6b306a2d1b
chestergarett <chestergarett@gmail.com>
2021-08-16 01:11:22 +0800

    alpha blog progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
   def new
     @user = User.new
   end
@@ -13,6 +22,21 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 
   private
 
